aec1cb832595efcf53589dcac022f728eb862405
vladimir <tarskij-vladimir@yandex.ru>
2024-02-16 11:57:09 +0300

    added Spearman

---
+++ Spearman.java
@@ -0,0 +1,12 @@
+// Копейщик
+
+public class Spearman extends Pers{
+    int strength;
+    int pritection;
+    public Spearman(int id, String name, int health, int stamina, String weapon, int strength, int pritection) {
+        super(id, name, health, stamina, weapon);
+        this.strength = strength;
+        this.pritection = pritection;
+    }
+    
+}
