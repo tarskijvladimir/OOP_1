839a358cac6a630081096f577a3dd4231d861fa9
vladimir <tarskij-vladimir@yandex.ru>
2024-02-20 20:33:09 +0300

    change Main

--- Main.java
+++ Main.java
@@ -73,13 +73,79 @@
 - чакра
  */
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         
-        Sniper sniper = new Sniper(0, null, 0, 0, null, 0, 0);
+        Sniper sniper = new Sniper(null, 0, 0);
+        Crossbowman crossbowman = new Crossbowman(null, 0, 0);
+        Magic magic = new Magic(null, 0, 0);
+        Monk monk = new Monk(null, 0, 0);
+        Peasant peasant = new Peasant(null, 0, 0);
+        Robber robber = new Robber(null, 0, 0);
+        Spearman spearman = new Spearman(null, 0, 0);
 
-        //sniper.
+        System.out.printf("Name: %s %s\n", sniper.name, sniper.toString());
+        System.out.printf("Name: %s %s\n", crossbowman.name, crossbowman.toString());
+        System.out.printf("Name: %s %s\n", magic.name, magic.toString());
+        System.out.printf("Name: %s %s\n", monk.name, monk.toString());
+        System.out.printf("Name: %s %s\n", peasant.name, peasant.toString());
+        System.out.printf("Name: %s %s\n", robber.name, robber.toString());
+        System.out.printf("Name: %s %s\n", spearman.name, spearman.toString());
+
+        int teamCount = 10;
+        
+        ArrayList<Pers> team1 = new ArrayList<>();
+        ArrayList<Pers> team2 = new ArrayList<>();
+
+        for (int i = 0; i < teamCount; i++){
+            switch (new Random().nextInt(2)) {
+                case 0:
+                    team1.add(new Sniper(getName(), i, 0));
+                    break;
+            
+                case 1:
+                    team1.add(new Robber(getName(), i,  0));
+                    break;
+            }
+            switch (new Random().nextInt(2)) {
+                case 0:
+                    team2.add(new Sniper(getName(), i, 9));
+                    break;
+            
+                case 1:
+                    team2.add(new Robber(getName(), i, 9));
+                    break;
+            }
+		// вывести имя и координаты
+          	for (int I = 0; I < 10;  i++){
+			System.out.println();
+		}         
+
+            // Random rand = new Random();   //  1
+            // int val = rand.nextInt(7);   // 2
+            // int r = rand.nextInt(10);
+            // switch (val) {
+            //     case 0:
+            //         teams.add(new Monk(getName());
+            //         break;
+            //     case 2:
+            //         teams.add(new Crossbowman(getName(), i, val));
+            
+            //     default:
+            //         break;
+            // }
+
+        }
+        
     }
+    private static String getName(){
+        return String.valueOf(Names.values()[new Random().nextInt(Names.values().length-1)]);
+}
 }
 
