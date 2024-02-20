f448ec2886e1147235601734869f4a1455508612
vladimir <tarskij-vladimir@yandex.ru>
2024-02-20 20:36:56 +0300

    change Robber

--- Robber.java
+++ Robber.java
@@ -1,10 +1,10 @@
 public class Robber extends Pers{
     int power;
     int agility;
-    public Robber(int id, String name, int health, int stamina, String weapon, int power, int agility) {
-        super(id, name, health, stamina, weapon);
-        this.power = power;
-        this.agility = agility;
+    public Robber(String name, int x, int y) {
+        super(5, name, 50, 40, 80, x, y);
+        // this.power = power;
+        // this.agility = agility;
     }
     
 }
