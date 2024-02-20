b7d636cb40572e06a3ef2c1c5a9cb4b0aaa8f61d
vladimir <tarskij-vladimir@yandex.ru>
2024-02-20 20:38:11 +0300

    change Spearman

--- Spearman.java
+++ Spearman.java
@@ -3,10 +3,10 @@
 public class Spearman extends Pers{
     int strength;
     int pritection;
-    public Spearman(int id, String name, int health, int stamina, String weapon, int strength, int pritection) {
-        super(id, name, health, stamina, weapon);
-        this.strength = strength;
-        this.pritection = pritection;
+    public Spearman(String name, int x, int y) {
+        super(7, name, 50, 40, 20, x, y);
+        // this.strength = strength;
+        // this.pritection = pritection;
     }
     
 }
