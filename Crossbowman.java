c8760eb29b626b21d068dfd759cd7f45f9c83388
vladimir <tarskij-vladimir@yandex.ru>
2024-02-28 17:27:15 +0300

    change Crossbowman

--- Crossbowman.java
+++ Crossbowman.java
@@ -1,12 +1,42 @@
-// Арбалетчик
 
-public class Crossbowman extends Pers{
-    int amountOfbowns;
-    int accuracy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// Арбалетчик. Своё свойство - количество стрел
+public class Crossbowman extends Pers {
+    int countArrow;
     public Crossbowman(String name, int x, int y) {
-        super(2, name, 50, 60, 100, x, y);
-        // this.amountOfbowns = amountOfbowns;
-        // this.accuracy = accuracy;
+        super(name, 150, "crossbow", 20, 4, 15, 50, 40, new Position(x,y));
+        countArrow = 10;
     }
-    
+
+    @Override
+    public String toString() {
+        return super.toString() + ", \u27b6 : " + countArrow;
+    }
+
+    /*
+    Доработать классы лучников. Лучник должен, во-первых, проверить жив ли он и есть ли у него стрелы,
+    если нет то завершить метод. Если всё да, то найти ближайшего противника и выстрелить по нему
+    и, соответственно потратить одну стрелу. Реализовать весь функционал лучников в методе step().
+     */
+    @Override
+    public void step(ArrayList<Pers> enemy, ArrayList<Pers> friend) {
+        if ((health<=0) || (countArrow == 0)) return;
+        Pers target = super.nearestEnemy(enemy);
+        target.getHit(this.powerHit);
+        countArrow--;
+
+
+
+
+    }
+
+    public String getInfo(){
+        return "Арбалетчик";};
+
+
+
 }
