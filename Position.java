69ad1090cd7a76347e328e9f50388e623caed374
vladimir <tarskij-vladimir@yandex.ru>
2024-02-28 17:32:56 +0300

    change Position

--- Position.java
+++ Position.java
@@ -1,30 +1,43 @@
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Position {
-    protected int x;
-    protected int y;
-
-    public Position(int x, int y) {
+    int x;
+    int y;
+    public Position(int x, int y){
         this.x = x;
         this.y = y;
     }
 
-    //
+    public void setPosition(int value1, int value2) {
+        this.x = value1;
+        this.y = value2;
+    }
     public int getX() {
         return x;
     }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
     public int getY() {
         return y;
     }
-
-    public void setY(int y) {
-        this.y = y;
+    @Override
+    public String toString(){
+        return x + "," + y;
     }
+
+
+    // Метод нахождения расстояния между двумя персонажами
+    public double distance(Position target){
+
+        double d = Math.sqrt(Math.pow(x - target.x, 2) + Math.pow(y - target.y, 2));
+
+        return d;
+    }
+
+    public Position getDifference(Position target) {
+        Position dif = new Position(x - target.x, y - target.y);
+        return dif;
+    }
+ public boolean equals (Position target){
+        return x == target.x && y == target.y;
+ }
+
+
+
 }
