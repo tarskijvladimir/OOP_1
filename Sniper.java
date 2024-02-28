4d5524813951c16ec3d7117462b6c3a174714e3c
vladimir <tarskij-vladimir@yandex.ru>
2024-02-28 17:34:13 +0300

    Sniper

--- Sniper.java
+++ Sniper.java
@@ -1,14 +1,29 @@
-// Снайпер
+import java.util.ArrayList;
 
-public class Sniper extends Pers{
-    int accuracy;
-    int disguise;
+// Снайпер. Своё свойство - нет уникальностей
+public class Sniper extends Pers {
+    int countArrow;
+
     public Sniper(String name, int x, int y) {
-        super(6, name, 50, 60, 80, x, y);
-        // this.accuracy = accuracy;
-        // this.disguise = disguise;
+
+        super(name, 150, "slingshot", 40, 2, 15, 50, 100, new Position(x, y));
+        countArrow = 20;
     }
 
-    
-    
+    @Override
+    public String toString() {
+        return super.toString() + ", \u27b6 : " + countArrow;
+    }
+
+    @Override
+    public void step(ArrayList<Pers> enemy, ArrayList<Pers> friend) {
+        if ((health <= 0) || (countArrow == 0)) return;
+        Pers target = super.nearestEnemy(enemy);
+        target.getHit(this.powerHit);
+        countArrow--;
+    }
+
+    public String getInfo(){
+        return "Снайпер";
+    }
 }
