636ed6f8c8e203009e824f28503d136132d4fd78
vladimir <tarskij-vladimir@yandex.ru>
2024-03-06 23:47:08 +0300

    change Peasant

--- Peasant.java
+++ Peasant.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 
 // Крестьянин. Своё свойство - количество стрел
+
 public class Peasant extends Pers {
     int countArrow;
+    protected boolean flag;
     public Peasant(String name, int x, int y) {
         super(name, 150, "arrow", 0, 4, 10, 0, 40, new Position(x,y));
         countArrow = 0;
+        flag = false;
     }
     protected void printname() {
         System.out.println("Name: " + name);
@@ -15,12 +18,21 @@ public class Peasant extends Pers {
 
     @Override
     public void step(ArrayList<Pers> enemy, ArrayList<Pers> friend) {
-
+        if (health <= 0) return;
+        flag = false;
     }
 
+    public int getCountArrow() {
+        return countArrow;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", \u27b6 : " + countArrow;
+    }
     public String getInfo(){
         return "Фермер";
     };
 }
+
     
 
